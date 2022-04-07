91bf081655fd95cffcfe3861edfc0fa0bd4cad05
d068098 <pascal.wasem@sap.com>
2022-04-07 14:09:38 +0200

    refactor!: api methods

--- .www/routes/table.js
+++ .www/routes/table.js
@@ -13,24 +13,28 @@ export default (tableService) => {
   const router = express.Router()
 
   // get game details
-  router.get('/', ({ user: { id: playerId } }, res, next) => {
-    const data = {
-      game: tableService.game(),
-      players: tableService.players().map(playerToObject),
-      currentPlayer: playerToObject(tableService.currentPlayer()),
-      communityCards: tableService.communityCards().map(cardToObject),
-      playerCards: tableService.playerCards(playerId).map(cardToObject),
-      bets: tableService.bets(),
-      pot: tableService.pot(),
-      winner: playerToObject(tableService.winner()),
-      winnerHand: tableService.winnerHand().map(cardToObject)
+  router.get('/', ({ user: { id } }, res, next) => {
+    const { game, players, currentPlayer, communityCards, bets, pot, winner, winnerHand } = tableService
+    const playerCards = tableService.getPlayerCards(id)
+    const snapshot = {
+      game,
+      players: players.map(playerToObject),
+      currentPlayer: playerToObject(currentPlayer),
+      communityCards: communityCards.map(cardToObject),
+      playerCards: playerCards.map(cardToObject),
+      bets,
+      pot,
+      winner: playerToObject(winner),
+      winnerHand: winnerHand.map(cardToObject)
     }
-    res.json(data)
+    res
+      .status(200)
+      .json(snapshot)
   })
 
   // join game
-  router.post('/players', ({ user: { id: playerId, name } }, res, next) => {
-    tableService.addPlayer({ id: playerId, name })
+  router.post('/players', ({ user: { id, name } }, res, next) => {
+    tableService.addPlayer({ id, name })
     res
       .status(204)
       .end()
